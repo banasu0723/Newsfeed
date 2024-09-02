bb7d4bed12e6c65ea1dd7933b78176df5665cc54
banasu0723 <banasu0723@gmail.com>
2024-09-02 16:36:54 +0900

    git init

---
+++ src/main/java/org/sparta/newsfeed/NewsfeedApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
