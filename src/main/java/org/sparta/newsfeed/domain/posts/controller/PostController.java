527930bd64604f716ee46cc36c3b806f5566a70c
banasu0723 <banasu0723@gmail.com>
2024-09-04 13:54:02 +0900

    [FIX] PostRepository 정렬

--- src/main/java/org/sparta/newsfeed/domain/posts/controller/PostController.java
+++ src/main/java/org/sparta/newsfeed/domain/posts/controller/PostController.java
@@ -4,22 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.sparta.newsfeed.domain.posts.dto.PostRequestDto;
 import org.sparta.newsfeed.domain.posts.dto.PostResponseDto;
 import org.sparta.newsfeed.domain.posts.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
 
-    @Autowired
-    private PostService postService;
+    private final PostService postService;
 
     @PostMapping
     public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto) {
         return postService.createPost(postRequestDto);
     }
+
+
 }
--- src/main/java/org/sparta/newsfeed/domain/posts/entity/Post.java
+++ src/main/java/org/sparta/newsfeed/domain/posts/entity/Post.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.sparta.newsfeed.domain.users.entity.User;
 
 import java.sql.Timestamp;
 
@@ -16,11 +17,9 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /*
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User id;
-    */
+    @JoinColumn(nullable = false)
+    private User user;
 
     @Column(nullable = false)
     private String title;
@@ -33,8 +32,8 @@ public class Post {
     private Timestamp updatedAt;
 
     @Builder
-    public Post(String title, String content, Timestamp createdAt, Timestamp updatedAt) {
-        //this.user = user;
+    public Post(User user, String title, String content, Timestamp createdAt, Timestamp updatedAt) {
+        this.user = user;
         this.title = title;
         this.content = content;
         this.createdAt = createdAt;
