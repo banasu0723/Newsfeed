ccac9168bceeee56f64486b032f4f2f89e5c40e7
fargoe <fargoewave@gmail.com>
2024-09-05 15:08:31 +0900

    [FIX] 비밀번호 변경 : 유효성 검사 변경

--- src/main/java/org/sparta/newsfeed/domain/profiles/dto/PasswordUpdateRequestDto.java
+++ src/main/java/org/sparta/newsfeed/domain/profiles/dto/PasswordUpdateRequestDto.java
@@ -14,8 +14,8 @@ public class PasswordUpdateRequestDto {
 
     @NotBlank(message = "새 비밀번호는 비워둘 수 없습니다.")
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-            message = "새 비밀번호는 최소 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다."
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "새 비밀번호는 최소 8자 이상이어야 하며, 영문자, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다."
     )
     private final String newPassword;
 }
